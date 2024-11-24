47b9d3bd95baba8c47dcbe9327e8a31382acc56b
Nikita <nikita.voloshin04@gmail.con>
2024-11-25 01:00:05 +0300

    Fixing style errors

--- a1.cpp
+++ a1.cpp
@@ -56,40 +56,42 @@ int main() {
   Circle c3(2, 1.5, 1.25);
 
   std::mt19937 gen(102345);
-  ld sx = 0, sy = 0;
-  ld ss = 3.2;
-
-  ld ex = 0.8, ey = 0.8;
-
-  ld sa = 1.2, sb = 1.2;
+  ld x_begin = 0;
+  ld y_begin = 0;
+  ld x_side_begin = 3.2;
+  ld y_side_begin = 3.2;
+  ld x_final = 0.8;
+  ld y_final = 0.8;
+  ld x_side_final = 1.2;
+  ld y_side_final = 1.2;
 
   std::ofstream ofs("result.txt");
-  std::ofstream ofs2("accur.txt");
+  std::ofstream ofs2("related-result.txt");
   ofs << std::setprecision(7);
-  ll steps = 6;
+  ll steps = 9;
 
   ld pi = std::atan2(0, -2);
   ld ideal = pi / 4.0 + 1.25 * std::asin(0.8) - 1;
 
   std::cout << pi << " " << ideal;
 
-  ld point_step = ex / steps;
-  ld sa_step = (sa - ss) / steps;
-  ld sb_step = (sb - ss) / steps;
-
+  ld point_step_x = x_final / steps;
+  ld point_step_y = y_final / steps;
+  ld s_x = (x_side_final - x_side_begin) / steps;
+  ld s_y = (y_side_final - y_side_begin) / steps;
   for (ll i = 0; i <= steps; ++i) {
-    ld x0 = point_step * i, y0 = point_step * i;
-    ld ai = ss + sa_step * i;
-    ld bi = ss + sb_step * i;
+    ld x0 = point_step_x * i + x_begin;
+    ld y0 = point_step_y * i + y_begin;
+    ld x_i = x_side_begin + s_x * i;
+    ld y_i = y_side_begin + s_y * i;
 
-    for (ll j = 100; j <= 1e5 + 100; j += 500) {
-      ld res = monteCarloArea(gen, c1, c2, c3, x0, y0, ai, bi, j);
-      ofs << "(" << ai * bi << ", " << j << ", " << res << ") ";
-      ofs2 << "(" << ai * bi << ", " << j << ", " << (res - ideal) / ideal << ") ";
+    for (ll j = 100; j <= 1e5; j += 500) {
+      ld res = monteCarloArea(gen, c1, c2, c3, x0, y0, x_i, y_i, j);
+      ofs << "(" << x_i * y_i << ", " << j << ", " << res << ") ";
+      ofs2 << "(" << x_i * y_i << ", " << j << ", " << (res - ideal) / ideal << ") ";
     }
 
-    ofs << "\n\n";
-    ofs2 << "\n\n";
+    ofs << "\n";
   }
 
   ofs.close();
